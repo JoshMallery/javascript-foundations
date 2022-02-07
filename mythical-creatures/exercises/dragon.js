f63ea48f5af85c139b5faef552da1f7ee063fb53
Josh Mallery <josh.mallery@gmail.com>
2022-02-06 23:38:45 -0700

    Add Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+  constructor(name,rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.hungerCount = 0
+  }
+greet(){
+  return `Hi, ${this.rider}!`
+}
 
+eat(){
+  this.hungerCount += +1
+  if(this.hungerCount === 3 ){
+    this.hungry = false
+
+  }
+}
+}
+
+module.exports = Dragon;
