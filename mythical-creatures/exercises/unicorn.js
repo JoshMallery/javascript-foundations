ae07e4ddefe138714ca9ce8af4b09f6ba8f6eb09
Josh Mallery <josh.mallery@gmail.com>
2022-02-06 17:54:35 -0700

    Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,25 @@
 class Unicorn {
+  //color: 'white',
   constructor(name, color) {
+
     this.name = name;
+    //this.color = 'white';
     this.color = color;
+    if(this.color === undefined){
+      this.color = 'white'
+    }
+  }
+
+  isWhite(){
+    if (this.color === 'white'){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  says(text){
+    return `**;* ${text} *;**`
   }
 }
 
