aae786eeb0958e13b402cfe08f65b6d96e848ddb
Josh Mallery <josh.mallery@gmail.com>
2022-02-08 23:51:16 -0700

    Add medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,26 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+constructor(name){
+this.name = name,
+this.statues = []
+  }
+
+  gazeAtVictim(victim) {
+if(this.statues.length < 3){
+    this.statues.push(new Statue(victim.name))
+  } else if (this.statues.length === 3){
+    this.statues.push(new Statue(victim.name));
+    
+   var person = new Person(this.statues[0].name);
+    person.mood = 'relieved';
+    this.statues.shift();
+  }
+  return person
+}
+
+}
+
+
+module.exports = Medusa
