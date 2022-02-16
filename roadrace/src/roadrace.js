e05686f46005febdfa32bdb7a41ed1dccd19a8e1
Josh Mallery <josh.mallery@gmail.com>
2022-02-15 17:57:33 -0700

    Add Roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,4 +1,24 @@
 class Roadrace {
+ constructor(roadrace) {
+   this.name = roadrace.title;
+   this.location = roadrace.city;
+   this.participants = [];
+ }
+
+  setDistance(distance) {
+    this.distance = distance;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      this.participants[i].runRace(this.name, this.distance);
+      }
+  }
 
 }
 
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,4 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = []
+  }
+
+  runSomeMiles(mileage) {
+    this.milesRun += mileage;
+    this.fitness += mileage;
+  }
+
+  stretch() {
+    this.fitness += 0.5;
+  }
+
+  runRace(name, mileage) {
+    this.completedRaces.push(name);
+    this.runSomeMiles(mileage);
+  }
 
 }
 
