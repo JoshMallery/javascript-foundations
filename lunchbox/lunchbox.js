adae7e515493243ec2e059d0c8b4e9db99540a03
Josh Mallery <josh.mallery@gmail.com>
2022-02-27 19:08:20 -0700

    Add Lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,4 +1,26 @@
 class LunchBox {
+  constructor(box) {
+    this.owner = box.owner;
+    this.material = box.madeOf;
+    this.shape = box.shape;
+    this.color = box.color;
+    this.snacks = [];
+  }
+
+  acquireSnack(snackItem) {
+    snackItem.isInLunchBox = true;
+    this.snacks.push(snackItem);
+  }
+
+  findHealthySnacks() {
+    var hSnacks =[];
+    for (var i =0; i < this.snacks.length; i++){
+      if(this.snacks[i].checkForHealthy()){
+        hSnacks.push(this.snacks[i].type);
+      }
+    }
+    return hSnacks;
+  }
 
 }
 
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,4 +1,29 @@
 class Snack {
+  constructor(item) {
+    this.type = item;
+    this.deliciousLevel = "extra";
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+
+  getEaten() {
+    if ( this.amount <= 30 ) {
+      this.cuttingItClose = true;
+    }
+
+    this.amount -= 10;
+  }
+
+  checkForHealthy() {
+    if(this.type.includes("fruit") || this.type.includes("Fruit")) {
+      return true;
+    }
+
+    return false;
+  }
+
+
 
 }
 
